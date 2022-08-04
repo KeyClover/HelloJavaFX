23b1770fde959adc8fc6819caa6c8bc672f13f35
KeyClover <suvichak0001@gmail.com>
2022-08-04 12:59:07 +0700

    HelloJavaFX first version

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module HelloJavaFX {
+
+    requires javafx.base;
+
+    requires javafx.controls;
+
+    requires javafx.graphics;
+
+    requires javafx.fxml;
+
+
+
+    opens org.stamford to javafx.fxml;
+
+    exports org.stamford;
+
+}
---
+++ src/main/java/org/stamford/mainApp.java
@@ -0,0 +1,55 @@
+package org.stamford;
+
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Parent;
+
+import javafx.scene.Scene;
+
+import javafx.stage.Stage;
+
+
+
+public class mainApp extends Application {
+
+
+
+    @Override
+
+    public void start(Stage stage) throws Exception {
+
+        String myClass = getClass().toString();
+
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+
+
+
+        Scene scene = new Scene(root);
+
+        scene.getStylesheets().add(getClass().getResource("styles.css")
+
+                .toExternalForm());
+
+
+
+        stage.setTitle("JavaFX and Gradle");
+
+        stage.setScene(scene);
+
+        stage.show();
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+    }
+
+
+
+}
